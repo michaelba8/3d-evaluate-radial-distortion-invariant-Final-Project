83252508ccf541da5c051010d52901a6adbcec33
michaelba8 <63546875+michaelba8@users.noreply.github.com>
2021-03-18 17:41:54 +0200

    apply distortion works

---
+++ applyDistortion.py
@@ -0,0 +1,45 @@
+import numpy as np
+import cv2
+from wand.image import Image
+import os
+
+def main():
+    path="projection results\car2/4.png"
+    imgs=read_images("projection results\iron man")
+
+    img = cv2.imread(path)
+    coef=(0.5,0,0,1)
+    newimg=apply_distortion_lib(path,coef)
+
+    # Display old and new image
+    cv2.imshow('old',img)
+    cv2.imshow('new',newimg)
+    cv2.waitKey()
+
+
+
+
+
+def apply_distortion_lib(path,coef=(0.5, 0.0, 0.0, 1.0)):
+    with Image(filename=path) as img:
+        print(img.size)
+        img.virtual_pixel = 'transparent'
+        img.distort('barrel', coef)
+        img_opencv = np.array(img)
+
+    return img_opencv
+
+
+def read_images(folder):
+    """Reading all the image from folder"""
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(filename,0)
+        if img is not None:
+            images.append(img)
+
+    return images
+
+
+if __name__=='__main__':
+    main()
